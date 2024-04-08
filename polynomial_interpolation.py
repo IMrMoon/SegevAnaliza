12fa2b957167c875e8c55191851cc4021391a85c
IMrMoon <sgvchen12@gmail.com>
2024-04-08 11:16:21 +0300

    dorit

--- polynomial_interpolation.py
+++ polynomial_interpolation.py
@@ -21,8 +21,8 @@ def polynomialInterpolation(table_points, x):
 
 
 if __name__ == '__main__':
-    table_points = [(0, 0), (1, 0.8415), (2, 0.9093), (3, 0.1411), (4, -0.7568), (5, -0.9589), (6, -0.2794)]
-    x = 1.28
+    table_points = [(1.2, 1.5095), (1.3, 1.6984), (1.4, 1.9043), (1.5, 2.1293), (1.6, 2.3756)]
+    x = 1.47
     print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
     print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
     print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x, '\n')
